bb241b324274ed7886f4226961af4bcc64752322
robin.herd <robin.herd@riskfirst.com>
2018-07-01 08:52:39 +0100

    add help comments to build-installer.bat

--- build-installer.bat
+++ build-installer.bat
@@ -1,1 +1,7 @@
+:: Before running build
+:: --------------------
+:: 1. Update version number here and in InstallerDefinition.iss
+:: 2. Ensure path to devenv.com is correct (changes with different versions of Visual Studio, e.g. 2017 Community)
+:: 2. Ensure `npm install` has been executed in WcfPad.UI/public/src
+
 gulp all --cwd WcfPad.UI/public/src && echo Starting devenv rebuild... && "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com" WcfPad.sln /Rebuild Release && "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" InstallerDefinition.iss
